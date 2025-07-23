e711cfb1f7b697b60a2e62c468b41270f05fcd62
Will Albeck <will@albeck.me>
2025-07-23 05:04:09 -0400

    Complete Tasklister lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // ✅ SAFER way to get the input:
+    const taskInput = document.querySelector("#new-task-description").value;
+
+    buildToDo(taskInput);
+    event.target.reset();
+  });
+
+  function buildToDo(task) {
+    const taskList = document.querySelector("#tasks");
+    const newTask = document.createElement("li");
+    newTask.textContent = task;
+    taskList.appendChild(newTask);
+  }
 });
